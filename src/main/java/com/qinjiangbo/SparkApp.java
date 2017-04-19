18eac0dfededadf219d33774c27c6f93f6167909
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2017-04-19 20:02:30 +0800

    add first spark app

---
+++ src/main/java/com/qinjiangbo/SparkApp.java
@@ -0,0 +1,38 @@
+package com.qinjiangbo;
+
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.SparkSession;
+
+/**
+ * @date: 19/04/2017 5:00 PM
+ * @author: qinjiangbo@github.io
+ */
+public class SparkApp {
+
+    public static void main(String[] args) {
+
+        // initialize spark session
+        SparkSession sparkSession = SparkSession
+                .builder()
+                .appName("Spark App")
+                // local[N] | spark://** | mesos|zk://** | simr://**
+                .master("local[2]")
+                .getOrCreate();
+
+        // initialize spark context
+        JavaSparkContext sparkContext =
+                new JavaSparkContext(sparkSession.sparkContext());
+
+        // we take the raw data in CSV format and convert it into
+        // a set of records of the form (user, product, price)
+
+        String filePath = System.getProperty("user.dir") +
+                "/src/main/resources/UserPurchaseHistory.csv";
+
+        JavaRDD<String[]> data = sparkContext.textFile(filePath)
+                .map(s -> s.split(","));
+
+
+    }
+}

b18ae41dddf8cf141c93f564305b5587177d1d86
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2017-04-23 23:55:32 +0800

    add MovieLens Items application

--- src/main/java/com/qinjiangbo/movies/MovieItemApp.java
+++ src/main/java/com/qinjiangbo/movies/MovieItemApp.java
@@ -3,10 +3,12 @@ package com.qinjiangbo.movies;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.PairFunction;
 import org.apache.spark.sql.SparkSession;
 import scala.Tuple2;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -34,9 +36,10 @@ public class MovieItemApp {
         System.out.println("Movies: " + items.count());
 
         JavaRDD<String[]> data = items.map(string -> string.split("\\|"));
-        JavaPairRDD<String, Integer> pairs = data
+        data = data.filter(strings -> strings[2].split("-").length == 3);
+        JavaRDD<String[]> years = data.map(strings -> strings[2].split("-"));
+        JavaPairRDD<String, Integer> pairs = years
                 .mapToPair(strings -> new Tuple2<>(strings[2], 1));
-
         List<Tuple2<String, Integer>> list = pairs
                 .reduceByKey((a , b) -> a + b).collect();
 

abfb8f3e62e8d29ec561a4e70f74e2eaf9c8b42b
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2017-05-19 16:26:41 +0800

    add RecommendationApp class for recommendation engine app

---
+++ src/main/java/com/qinjiangbo/recommendation/RecommendationApp.java
@@ -0,0 +1,12 @@
+package com.qinjiangbo.recommendation;
+
+/**
+ * @date: 19/05/2017 4:25 PM
+ * @author: qinjiangbo@github.io
+ */
+public class RecommendationApp {
+
+    public static void main(String[] args) {
+        System.out.println("This is a recommendation application.");
+    }
+}
